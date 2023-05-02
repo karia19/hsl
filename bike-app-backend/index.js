b8aec70c2caa84c59890fbd2ffd4097c842b4de2
karia19 <39730014+karia19@users.noreply.github.com>
2023-05-02 15:02:20 +0300

    store

--- bike-app-backend/index.js
+++ bike-app-backend/index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const jorneyRoutes = require('./routes/jorneyRoutes');
 const stationsRoutes = require('./routes/stationRoutes');
+const storeRoures = require('./routes/storeNewRoutes');
 const mongoose = require('mongoose');
+const mongoApp = require('./utils/mongoApp');
+
 const mongoUri = `mongodb://karppa:karppa2023@0.0.0.0:27017?authSource=admin`
 
 //console.log(mongoUri)
 
+/// CONNECT TO MONGO DOCKER ///
+/*
 mongoose.connect(mongoUri,  {useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log('connection is working')
@@ -14,6 +19,8 @@ mongoose.connect(mongoUri,  {useNewUrlParser: true, useUnifiedTopology: true })
     .catch((error) => {
         console.log('error hapened', error )
 })
+*/
+mongoApp.MongoConnect()
 
 
 const app = express()
@@ -24,8 +31,11 @@ app.get("/", (req, res) => {
     res.status(200).json({ message: "working node app"})
 })
 
-app.use("/api/v1/jorneys/", jorneyRoutes)
-app.use("/api/v1/stations/", stationsRoutes)
+/// ROUTES TO REST API ///
+app.use("/api/v1/jorneys/", jorneyRoutes);
+app.use("/api/v1/stations/", stationsRoutes);
+app.use("/api/v1/store/", storeRoures);
+
 
 const port = process.env.PORT || 3003
 app.listen(port, () => console.log(`server is running ${port}`))
