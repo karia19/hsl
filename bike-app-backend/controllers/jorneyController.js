9db1fd577634f24113735d28e97cd106b2bbd37e
karia19 <39730014+karia19@users.noreply.github.com>
2023-05-10 10:13:08 +0300

    Update jorneyController.js

--- bike-app-backend/controllers/jorneyController.js
+++ bike-app-backend/controllers/jorneyController.js
@@ -1,11 +1,6 @@
 const jorneyData = require('../models/journeyData');
 const math = require('mathjs');
-const moment = require('moment');
-const statsCal = require('../utils/statsCal');
-const { toDependencies } = require('mathjs');
-
-
-
+const statCalculator = require('../utils/statCalculator');
 
 /// get jorney by id ///
 
@@ -49,57 +44,33 @@ exports.getFifty = async (req, res, next) => {
         res.sendStatus(500).json({ message: "err in get...."})
     }
 }
-/// find departure stations data by station and calculate some stats ///
+/// find departure stations data by station and calculate some stats by month if moth is over 12 return all data by station ///
 exports.getByDepartureStation = async ( req, res, next ) => {
     try {
         const stationToFind = req.query.name
-        console.log(stationToFind)
-        const fromDbByStation = await jorneyData.find({ DepartureStationName: stationToFind })
-        const returnByStation = await jorneyData.find({  ReturnStationName: stationToFind })
-      
-        try {
-            /// stats from departure station ///
-            const meanDistance = math.mean(fromDbByStation.map(x => x['CoveredDistance']))
-            const maxDistance = Math.max(...fromDbByStation.map(x => x['CoveredDistance']))
-
-            const meanDuration = math.mean(fromDbByStation.map(x => x['Duration']))
-            const maxDuration = Math.max(...fromDbByStation.map(x => x['Duration']))
-     
-            /// stations from return station /// 
-            const returnMeanDistance = math.mean(returnByStation.map(x => x['CoveredDistance']))
-            const returnMaxDistance = Math.max(...returnByStation.map(x => x['CoveredDistance']))
-
-            const returnMeanDuration = math.mean(returnByStation.map(x => x['Duration']))
-            const returnMaxDuration = Math.max(...returnByStation.map(x => x['Duration']))
-
-            res.json({
-                departureResults: fromDbByStation.length,
-                departureMeanDistance: meanDistance / 1000,
-                departureMaxDistance: maxDistance / 1000,
-                departureMeanDuration: meanDuration / 60,
-                departureMaxDuration: maxDuration / 60,
-                departureMeanSpeed: (meanDistance / 1000) / (meanDuration / 3600),
+        const month = Number(req.query.month)
+        if (month <= 12){
+                const fromDbByStation = await jorneyData.aggregate([
+                    // Find stations  and month //
+                    { $match: { DepartureStationName: stationToFind}},   
+                    { $addFields: { month: { "$month": {$toDate: "$Departure"}}},},
+                    { $match : { "month": month }},
                 
-                retrunResults: returnByStation.length,
-                returnMeanDistance: returnMeanDistance / 1000,
-                returnMaxDistance: returnMaxDistance / 1000,
-                returnMeanDuration: returnMeanDuration / 60,
-                returnMaxDuration: returnMaxDuration / 60,
-                returnMeanSpeed: (returnMeanDistance / 1000) / (returnMeanDuration / 3600),
-
-                data: fromDbByStation.slice(0, 50)
-            })
-            
-        } catch(e) {
-            res.json({
-                results: fromDbByStation.length,
-                meanDistance: 0,
-                maxDistance: 0,
-                meanDuration: 0,
-                maxDuration: 0,
-                meanSpeed: 0,
-                data: fromDbByStation.slice(0, 50)
-            })
+                ])
+                const returnByStation = await jorneyData.aggregate([
+                    // Find stations and month //
+                    { $match: { ReturnStationName: stationToFind}},
+                    { $addFields: { month: { "$month": {$toDate: "$Departure"}}},},
+                    { $match : { "month": month }},     
+                ])
+                const modData = statCalculator.convert(fromDbByStation, returnByStation)                
+                res.json(modData)
+        } else {
+                const fromDbByStation = await jorneyData.aggregate([{ $match: { DepartureStationName: stationToFind}},])
+                const returnByStation = await jorneyData.aggregate([{ $match: { ReturnStationName: stationToFind}},])
+                const modData = statCalculator.convert(fromDbByStation, returnByStation)
+                
+                res.json(modData)
 
         }
 
@@ -107,67 +78,7 @@ exports.getByDepartureStation = async ( req, res, next ) => {
         res.sendStatus(500).json({ message: "error..."})
     }
 
-}
-
-/// find departure stations data by station and month then calculate some stats ///
-exports.getByDepartureStationByMonth = async ( req, res, next ) => {
-    try {
-        const stationToFind = req.query.name
-        const monthToFind = req.query.month
-        const departureMonth = await jorneyData.find({ DepartureStationName: stationToFind })
-        const fromDbByStation = departureMonth.filter(x => moment(x.Departure).format("M") == monthToFind)
-        const returnMonth = await jorneyData.find({ ReturnStationName: stationToFind })
-        const returnByStation = returnMonth.filter(x => moment(x.Return).format("M") == monthToFind)
-
-        try {
-            /// stats from departure station ///
-            const meanDistance = math.mean(fromDbByStation.map(x => x['CoveredDistance']))
-            const maxDistance = Math.max(...fromDbByStation.map(x => x['CoveredDistance']))
-
-            const meanDuration = math.mean(fromDbByStation.map(x => x['Duration']))
-            const maxDuration = Math.max(...fromDbByStation.map(x => x['Duration']))
-     
-            /// stations from return station /// 
-            const returnMeanDistance = math.mean(returnByStation.map(x => x['CoveredDistance']))
-            const returnMaxDistance = Math.max(...returnByStation.map(x => x['CoveredDistance']))
-
-            const returnMeanDuration = math.mean(returnByStation.map(x => x['Duration']))
-            const returnMaxDuration = Math.max(...returnByStation.map(x => x['Duration']))
-
-            res.json({
-                departureResults: fromDbByStation.length,
-                departureMeanDistance: meanDistance / 1000,
-                departureMaxDistance: maxDistance / 1000,
-                departureMeanDuration: meanDuration / 60,
-                departureMaxDuration: maxDuration / 60,
-                departureMeanSpeed: (meanDistance / 1000) / (meanDuration / 3600),
-                
-                retrunResults: returnByStation.length,
-                returnMeanDistance: returnMeanDistance / 1000,
-                returnMaxDistance: returnMaxDistance / 1000,
-                returnMeanDuration: returnMeanDuration / 60,
-                returnMaxDuration: returnMaxDuration / 60,
-                returnMeanSpeed: (returnMeanDistance / 1000) / (returnMeanDuration / 3600),
-
-                data: fromDbByStation.slice(0, 50)
-            })
-        } catch(e) {
-            res.json({
-                results: fromDbByStation.length,
-                meanDistance: 0,
-                maxDistance: 0,
-                meanDuration: 0,
-                maxDuration: 0,
-                meanSpeed: 0,
-                data: fromDbByStation.slice(0, 50)
-            })
-
-        }
-        
-    } catch(e){
-        res.sendStatus(500).json({ message: "error..."})
-    }
-}
+} 
 
 /// get 50 longest distance ///
 exports.getLongestDistance = async (req, res, next) => {
