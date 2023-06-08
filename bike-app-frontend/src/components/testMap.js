6e94a6cadda396554ebd511b800c5326c33347ac
karia19 <39730014+karia19@users.noreply.github.com>
2023-06-08 11:49:36 +0300

    Update testMap.js

--- bike-app-frontend/src/components/testMap.js
+++ bike-app-frontend/src/components/testMap.js
@@ -75,7 +75,7 @@ const TestMapp = () => {
         <Map
             {... viewport}
             onMove={evt => setViewPort(evt.viewState)}
-            mapboxAccessToken= "pk.eyJ1Ijoia2FyaWEyMDIzIiwiYSI6ImNsZ3JpZHh2czBuNDczcG1reDFrNWJpeXoifQ.xu3lRPVRcWM-MAmL3FCByw" // {process.env.REACT_APP_MAPBOX_TOKEN}}
+            mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}}
             style={{width: '100%', height: '110vh',  }}
 
             mapStyle="mapbox://styles/karia2023/clgxbaxd700cg01qy8i0u95fx"
